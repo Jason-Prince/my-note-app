90b7ffd58533e3a025f545bf4080ecb42a8ce571
Jason Prince <jasoncprince@gmail.com>
2020-05-16 14:07:32 -0600

    routes protected by jwt

--- server/route/user-route.js
+++ server/route/user-route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const userController = require("../controller/user-controller");
+const jwtAuth = require("../middleware/user-auth");
 
 const router = express.Router();
 
@@ -8,13 +9,13 @@ router.route("/register").post(userController.register);
 router.route("/login").post(userController.login);
 router
   .route("/:userID/note")
-  .post(userController.makeNote)
-  .get(userController.getAllMyNotes);
+  .post(jwtAuth, userController.makeNote)
+  .get(jwtAuth, userController.getAllMyNotes);
 
 router
   .route("/:userID/note/:noteID")
-  .get(userController.getMyOneNote)
-  .patch(userController.updateNote)
-  .delete(userController.deleteNote);
+  .get(jwtAuth, userController.getMyOneNote)
+  .patch(jwtAuth, userController.updateNote)
+  .delete(jwtAuth, userController.deleteNote);
 
 module.exports = router;
